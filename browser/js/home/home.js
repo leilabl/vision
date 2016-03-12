df17b556be8098d4385826ef5ac1c2117e17e988
leilabl <leilaloezer@gmail.com>
2016-03-12 18:49:03 -0500

    basic functionality

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -6,7 +6,8 @@ app.config(function ($stateProvider) {
     });
 });
 
-app.controller('homeCtrl', function ($scope) {
+app.controller('homeCtrl', function ($scope, HomeFactory) {
+	// $scope.imgData = '';
 	$scope.cameraOn = function () {
         function userMedia(){
             return navigator.getUserMedia = navigator.getUserMedia ||
@@ -26,10 +27,8 @@ app.controller('homeCtrl', function ($scope) {
 
             var media = navigator.getUserMedia(constraints, function(stream){
 
-                // URL Object is different in WebKit
                 var url = window.URL || window.webkitURL;
 
-                // create the url and set the source of the video element
                 video.src = url ? url.createObjectURL(stream) : stream;
 
                 // Start the video
@@ -46,16 +45,79 @@ app.controller('homeCtrl', function ($scope) {
             console.log("KO");
         }
 
-        setTimeout(function() {
-        	var canvas = document.getElementById('canvas');
-                canvas.width = video.videoWidth;
-                canvas.height = video.videoHeight;
-                canvas.getContext('2d').drawImage(video, 0, 0);
-                var data = canvas.toDataURL('image/jpeg');
-                console.log('!!!!!!!!!!!!!!!!', data)
-                // document.getElementById('photo').setAttribute('src', data);
-        }, 3000);
+        $scope.joy = '';
 
+        // while($scope.joy !== 'VERY_LIKELY') {
+
+	        setTimeout(function() {
+	        	var canvas = document.getElementById('canvas');
+	                canvas.width = video.videoWidth;
+	                canvas.height = video.videoHeight;
+	                canvas.getContext('2d').drawImage(video, 0, 0);
+	                var data = canvas.toDataURL('image/jpeg');
+	                // console.log(data)
+	                HomeFactory.sendToVision(data)
+	                .then(function (response) {
+	                	$scope.labels = response.labelAnnotations;
+	                	$scope.firstLabel = response.labelAnnotations[0].description;
+	                	$scope.secondLabel = response.labelAnnotations[1].description;
+	                	var faceInfo = response.faceAnnotations[0];
+	                	$scope.anger = faceInfo.angerLikelihood;
+	                	$scope.joy = faceInfo.joyLikelihood;
+	                	console.log($scope.joy)
+	                	$scope.sorrow = faceInfo.sorrowLikelihood;
+	                	$scope.surprise = faceInfo.surpriseLikelihood;
+				        if ($scope.joy === 'VERY_UNLIKELY') {
+				        	$scope.firstMessage = 'You look sad, but you have a nice ' + $scope.firstLabel;
+				        }
+				        if ($scope.joy === 'UNLIKELY') {
+				        	$scope.secondMessage = 'Looks like you are not too sad... Great ' + $scope.secondLabel + ' by the way!';
+				        }
+				        if ($scope.joy === 'LIKELY') {
+				        	$scope.thirdMessage = 'I think you are getting happier';
+				        }
+				        if ($scope.joy === 'POSSIBLE') {
+				        	$scope.thirdMessage = 'Smile, please?';
+				        }
+				        if ($scope.joy === 'VERY_LIKELY') {
+				        	$scope.fourthMessage = 'Awww... beautiful smile';
+				        	video.pause();
+	                		videoPlaying  = false;
+				        }
+	                })
+	                // document.getElementById('photo').setAttribute('src', data);
+	        }, 3000);
+        // }
 
 	}
 })
+
+
+app.factory('HomeFactory', function ($http) {
+	var HomeFactory = {};
+
+	HomeFactory.sendToVision = function (imgData) {
+		return $http.post('/api/vision/', {imgData: imgData})
+		.then(function(data){
+			// console.log('here', data.data.responses[0])
+			return data.data.responses[0];
+        /*called for result & error because 200 status*/
+        if (data.result){
+        	console.log('success', data)
+            //handle success here
+        } else if (data.error) {
+        	console.log('data error')
+            //handle error here
+	    }
+	})
+	}
+	
+
+	return HomeFactory;
+})
+
+
+//check how to pass response to from factory to controller
+
+//look for funny quotes
+
